0288fa86305355ddf74c0e4ddcae925705c02578
duyle1294 <101852458+duyle1294@users.noreply.github.com>
2022-07-02 08:12:12 +0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,309 @@
+let allMusic = [
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Schor",
+        lyric: `1
+        these roads intersect, on each one i hear it flex
+        these lines ride my story
+        these places change me
+        each one replacing
+        like night into morning
+        i miss the opening
+        the going anywhere
+        riding with you
+        like we used too
+        
+        [chorus]
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        
+        [beat-b-ss – jordan schor & harley bird]
+        
+        (homes where the heart is)
+        
+        [verse 2]
+        the end, this journey
+        i need not worry
+        i’ll get where i’m going
+        i made it this far
+        beyond where you are, and i’ll keep on growing
+        
+        [chorus 2]
+        i miss the opening
+        the going anywhere
+        riding with you
+        like we used too
+        
+        
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        
+        (homes where the heart is)`,
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Ikson Anywhere – Ikson",
+        artist: "Audio Library",
+        lyric: `2
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        
+        [beat-b-ss – jordan schor & harley bird]
+        
+        (homes where the heart is)
+        
+        [verse 2]
+        the end, this journey
+        i need not worry
+        i’ll get where i’m going
+        i made it this far
+        beyond where you are, and i’ll keep on growing
+        
+        [chorus 2]
+        i miss the opening
+        the going anywhere
+        riding with you
+        like we used too
+        
+        
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        
+        (homes where the heart is)`,
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Beauz & Jvna - Crazy",
+        artist: "Beauz & Jvna",
+        lyric: `[verse]
+        these roads intersect, on each one i hear it flex
+        these lines ride my story
+        these places change me
+        each one replacing
+        like night into morning
+        i miss the opening
+        the going anywhere
+        riding with you
+        like we used too
+        
+        [chorus]
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        
+        [beat-b-ss – jordan schor & harley bird]
+        
+        (homes where the heart is)
+        
+        [verse 2]
+        the end, this journey
+        i need not worry
+        i’ll get where i’m going
+        i made it this far
+        beyond where you are, and i’ll keep on growing
+        
+        [chorus 2]
+        i miss the opening
+        the going anywhere
+        riding with you
+        like we used too
+        
+        
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        
+        (homes where the heart is)`,
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Hardwind - Want Me",
+        artist: "Mike Archangelo",
+        lyric: `
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        these roads intersect, on each one i hear it flex
+        these lines ride my story
+        these places change me
+        each one replacing
+        like night into morning
+        i miss the opening
+        the going anywhere
+        riding with you
+        like we used too
+        
+        [chorus]
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        
+        [beat-b-ss – jordan schor & harley bird]
+        
+        (homes where the heart is)
+        
+        [verse 2]
+        the end, this journey
+        i need not worry
+        i’ll get where i’m going
+        i made it this far
+        beyond where you are, and i’ll keep on growing
+        
+        [chorus 2]
+        i miss the opening
+        the going anywhere
+        riding with you
+        like we used too
+        
+        
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        
+        (homes where the heart is)`,
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Jim - Sun Goes Down",
+        artist: "Jim Yosef x Roy",
+        lyric: `[verse]
+        these roads intersect, on each one i hear it flex
+        these lines ride my story
+        these places change me
+        each one replacing
+        like night into morning
+        i miss the opening
+        the going anywhere
+        riding with you
+        like we used too
+        
+        [chorus]
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        
+        [beat-b-ss – jordan schor & harley bird]
+        
+        (homes where the heart is)
+        
+        [verse 2]
+        the end, this journey
+        i need not worry
+        i’ll get where i’m going
+        i made it this far
+        beyond where you are, and i’ll keep on growing
+        
+        [chorus 2]
+        i miss the opening
+        the going anywhere
+        riding with you
+        like we used too
+        
+        
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        
+        (homes where the heart is)`,
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NCS Release",
+        lyric: `
+        3
+        the end, this journey
+        i need not worry
+        i’ll get where i’m going
+        i made it this far
+        beyond where you are, and i’ll keep on growing
+        these roads intersect, on each one i hear it flex
+        these lines ride my story
+        these places change me
+        each one replacing
+        like night into morning
+        i miss the opening
+        the going anywhere
+        riding with you
+        like we used too
+        
+        [chorus]
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        
+        [beat-b-ss – jordan schor & harley bird]
+        
+        (homes where the heart is)
+        
+        [verse 2]
+        the end, this journey
+        i need not worry
+        i’ll get where i’m going
+        i made it this far
+        beyond where you are, and i’ll keep on growing
+        
+        [chorus 2]
+        i miss the opening
+        the going anywhere
+        riding with you
+        like we used too
+        
+        
+        guess i’m better off alone
+        never know when i’ll get home
+        but i know that homes where the heart is
+        guess i’m better off this way
+        moving to better days
+        but i know that homes where the heart is
+        
+        (homes where the heart is)`,
+        img: "music-6",
+        src: "music-6"
+    },
+];
